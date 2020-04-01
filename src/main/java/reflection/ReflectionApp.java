b58c486d480289b7404574f9aebe798ccbe8e40b
emokeeva <ekaterina.mokeeva@t-systems.com>
2020-04-01 21:17:38 +0300

    1

--- src/main/java/reflection/ReflectionApp.java
+++ src/main/java/reflection/ReflectionApp.java
@@ -10,14 +10,13 @@ import java.lang.reflect.Method;
 public class ReflectionApp {
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, InstantiationException {
         Car carObj = new Car("Rio");
-        Class <?> carClass = carObj.getClass();
-        Class <?> carClass1 = Car.class;   //одинаково
+        Class<?> carClass = carObj.getClass();
+        // LIKE Class<?> carClass1 = Car.class;   //одинаково
         carClass.getFields();
 
         Field[] publicFields = carClass.getFields();
-        for (Field publicField: publicFields)
-        {
-            System.out.println(publicField.getName());
+        for (Field publicField : publicFields) {
+            System.out.println("Public Fields" + publicField.getName());
         }
         Field modelField = carClass.getDeclaredField("model");
         System.out.println(modelField.getName());
@@ -26,20 +25,20 @@ public class ReflectionApp {
         System.out.println(carObj.getModel());
 
 //!!!!!тут ошибка, исправить
-       // Method changeModelM = carClass.getDeclaredMethod("changeModel", String.class, String.class);
-        //changeModelM.setAccessible(true);
-        //carObj.changeModel("Polo")
-     //   changeModelM.invoke(carObj, "Polo");
+        Method changeModelM = carClass.getDeclaredMethod("changeModel", String.class, String.class);
+        changeModelM.setAccessible(true);
+        // LIKE carObj.changeModel("Polo");
+        changeModelM.invoke(carObj, "Polo");
 
         Constructor<?> carConstructor = carClass.getDeclaredConstructor(String.class, String.class);
         carConstructor.setAccessible(true);
 
-       Car refcar = (Car) carConstructor.newInstance("l", "G");
-       //newInstance выполнение конструктора
-        System.out.println(refcar.getBrand()+" "+ refcar.getModel());
+        Car refcar = (Car) carConstructor.newInstance("l", "G");
+        //newInstance выполнение конструктора
+        System.out.println(refcar.getBrand() + " " + refcar.getModel());
 
         Car car = new Car("Polo");
         RandomStringAnnotationProcessor.process(car);
-        System.out.println(car.getBrand()+" "+car.getModel());
+        System.out.println(car.getBrand() + " " + car.getModel());
     }
 }

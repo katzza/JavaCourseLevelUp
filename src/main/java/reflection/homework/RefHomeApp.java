0c162b6bec136e17589a05565e2745b394049ed4
emokeeva <ekaterina.mokeeva@t-systems.com>
2020-04-03 16:59:43 +0300

    1

--- src/main/java/reflection/homework/RefHomeApp.java
+++ src/main/java/reflection/homework/RefHomeApp.java
@@ -1,9 +1,16 @@
 package reflection.homework;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class RefHomeApp {
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    //    File file1 = new File("RefHomeApp");
+   //    String sss = file1.getAbsolutePath().toString() ;
+
+
         String pathName = "C:\\Users\\emokeeva\\IdeaProjects\\pr2\\src\\main\\java\\ru\\levelup\\lessons";
         ArrayList<String> files = new ArrayList<>();
         ScanningFiles s = new ScanningFiles();
